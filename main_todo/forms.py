30688360656df0dadabc2f2e49051e57c48d4d67
solomzi-msj <solomzisithole183@gmail.com>
2021-09-06 15:34:27 +0200

    Added a new column and updated the index page

--- main_todo/forms.py
+++ main_todo/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, BooleanField
 from wtforms.validators import DataRequired
 
 class TodoField(FlaskForm):
     todo_input = StringField(validators=[DataRequired()])
+    completed = BooleanField('Complete', validators=[DataRequired()])
     submit = SubmitField('Add Todo')
