a74477c4471814cfd4a5d1cef0e43634c481f0ad
solomzi-msj <solomzisithole183@gmail.com>
2021-09-14 11:25:08 +0200

    Added search functionality to the app

--- main_todo/static/todo.js
+++ main_todo/static/todo.js
@@ -1,4 +1,5 @@
 const todos = document.querySelector('ul#todos');
+const searchInput = document.querySelector('#search');
 
 todos.addEventListener('click', e => {
     if(e.target.tagName === 'LI') {
@@ -6,4 +7,16 @@ todos.addEventListener('click', e => {
     } else if (e.target.tagName === 'SPAN') {
         e.target.parentElement.classList.toggle('complete');
     }
+});
+
+// searching Todos
+const tasks = document.querySelectorAll('ul li span');
+searchInput.addEventListener('keyup',() => {
+    tasks.forEach(task => {
+        if(!task.textContent.includes(searchInput.value)) {
+            task.parentElement.classList.add('d-none');
+        } else {
+            task.parentElement.classList.remove('d-none');
+        }
+    });
 });
